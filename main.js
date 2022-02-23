4a13a39153bf6eb796a8b4107174d5ff4b246c69
mutsa <Danobez21@gmail.com>
2022-02-23 20:37:55 +0300

    dom_task2 create select with options

---
+++ main.js
@@ -0,0 +1,44 @@
+// let listOp = [];
+// let select = document.querySelector(".drowing");
+// for (let i = 0; i < 10; i++) {
+//   listOp[i] = document.createElement("option");
+//   listOp[i].textContent = `${i}`;
+// }
+// select.append(listOp);
+
+arr1 = [
+  { value: "0", label: "first" },
+  { value: "1", label: "second" },
+  { value: "2", label: "third" },
+];
+arr2 = [1, 2, "three", "four", { value: "5", label: "third" }];
+arr3 = [
+  { value: 0 },
+  { label: "one" },
+  { glhf: "good game", someSpace: "i need" },
+];
+function selectCreate(arr, value = 0) {
+  let select = document.createElement("select");
+  let options = [];
+  for (let i = 0; i < arr.length; i++) {
+    options[i] = document.createElement("option");
+    options[i].setAttribute("value", arr[i].value);
+    options[i].textContent = arr[i].label;
+    select.append(options[i]);
+  }
+  select.selectedIndex = value;
+  return select;
+}
+
+function checkArrOnObj(arr) {
+  if (typeof arr === "object") {
+    for (let i = 0; i < arr.length; i++) {
+      if (typeof arr[i] !== "object") {
+        arr[i] = { value: `${arr[i]}`, label: `${arr[i]}` };
+      }
+    }
+  }
+  return arr;
+}
+
+document.body.append(selectCreate(checkArrOnObj(arr2), 1));
