3e89f59aa6b7e01774e739aa26d32393aaf588ea
mutsa <Danobez21@gmail.com>
2022-02-23 20:56:28 +0300

    something changed

--- main.js
+++ main.js
@@ -5,18 +5,32 @@
 //   listOp[i].textContent = `${i}`;
 // }
 // select.append(listOp);
-
-arr1 = [
+"use strict";
+let arr1 = [
   { value: "0", label: "first" },
   { value: "1", label: "second" },
   { value: "2", label: "third" },
 ];
-arr2 = [1, 2, "three", "four", { value: "5", label: "third" }];
-arr3 = [
+let arr2 = [1, 2, "three", "four", { value: "5", label: "third" }];
+let arr3 = [
   { value: 0 },
   { label: "one" },
   { glhf: "good game", someSpace: "i need" },
 ];
+
+function checkArrOnObj(arr) {
+  if (typeof arr === "object") {
+    for (let i = 0; i < arr.length; i++) {
+      if (typeof arr[i] !== "object") {
+        arr[i] = { value: `${arr[i]}`, label: `${arr[i]}` };
+      } else {
+        continue;
+      }
+    }
+  }
+  return arr;
+}
+
 function selectCreate(arr, value = 0) {
   let select = document.createElement("select");
   let options = [];
@@ -28,17 +42,6 @@ function selectCreate(arr, value = 0) {
   }
   select.selectedIndex = value;
   return select;
-}
-
-function checkArrOnObj(arr) {
-  if (typeof arr === "object") {
-    for (let i = 0; i < arr.length; i++) {
-      if (typeof arr[i] !== "object") {
-        arr[i] = { value: `${arr[i]}`, label: `${arr[i]}` };
-      }
-    }
-  }
-  return arr;
 }
 
 document.body.append(selectCreate(checkArrOnObj(arr2), 1));
