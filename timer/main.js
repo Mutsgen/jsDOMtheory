891f6734e69573da439114e471dcbd2ec9f1b8ff
mutsa <Danobez21@gmail.com>
2022-03-14 08:12:08 +0300

    pause for timer

--- timer/main.js
+++ timer/main.js
@@ -1,8 +1,11 @@
-"use strick";
+"use strict";
 const buttons = document.querySelector(".timer_buttons");
+const startBtn = document.querySelector(".start_timer");
+const stopBtn = document.querySelector(".stop_timer");
 const minutes = document.querySelector(".minutes");
 const seconds = document.querySelector(".seconds");
 let intervalId = 0;
+
 function addTimerValue(object, value) {
   if (object === seconds) {
     if (
@@ -31,8 +34,8 @@ function minusTimerValue() {
     seconds.innerText = 59;
     minutes.innerText = parseInt(minutes.innerText) - 1;
   } else {
-    alert("Panic!!! Time is over!!!");
     clearInterval(intervalId);
+    alert("Panic!!! Time is over!!!");
     minutes.innerText = "00";
     seconds.innerText = "00";
   }
@@ -50,10 +53,17 @@ buttons.addEventListener("click", function (event) {
   } else if (event.target.classList.contains("add_second")) {
     addTimerValue(seconds, 1);
   } else if (event.target.classList.contains("clear_timer")) {
+    clearInterval(intervalId);
     minutes.innerText = "00";
     seconds.innerText = "00";
+  } else if (event.target.classList.contains("stop_timer")) {
+    clearInterval(intervalId);
+    startBtn.style.display = "block";
+    stopBtn.style.display = "none";
   } else {
     clearInterval(intervalId);
     intervalId = setInterval(minusTimerValue, 1000);
+    startBtn.style.display = "none";
+    stopBtn.style.display = "block";
   }
 });
